bdceca949cafecc929afc4b1d707552656b022dc
Rodrigo José Carvalho de Mello <rodrigojcmello@gmail.com>
2018-08-15 10:27:54 -0300

    recorte estático

--- Componente.jsx
+++ Componente.jsx
@@ -7,16 +7,28 @@ class App extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
+            srcCorners: [191, 505, 741, 505, 147, 858, 793, 855]
         };
         this.canvas = createRef();
+        this.novoCanvas = createRef();
     }
     gerarCanvas() {
         this.contexto = this.canvas.current.getContext('2d');
-        var img = new Image();
-        img.src = imagem;
-        img.onload = () => {
-            this.contexto.drawImage(img, 0, 0);
+        this.img = new Image();
+        this.img.src = imagem;
+        this.img.onload = () => {
+            this.contexto.drawImage(this.img, 0, 0);
         };
+    }
+    obterCoordenada = (evento) => {
+        let x = evento.nativeEvent.layerX;
+        let y = evento.nativeEvent.layerY;
+        console.log(x, y);
+    }
+    cortarImagem = () => {
+        console.log('​App -> cortarImagem');
+        this.contexto2 = this.novoCanvas.current.getContext('2d');
+        this.contexto2.drawImage(this.img, -147, -505, 960, 1280);
     }
     componentDidMount() {
         this.gerarCanvas();
@@ -30,10 +42,23 @@ class App extends PureComponent {
     render() {
         return (
             <div>
+                <button
+                    onClick={this.cortarImagem}
+                >
+                    Cortar Imagem
+                </button>
                 <canvas
+                    ref={this.novoCanvas}
+                    height={858 - 505}
+                    width={793 - 147}
+                >
+                </canvas>
+                <canvas
+                    className='canvas'
                     ref={this.canvas}
                     height='1280'
                     width='960'
+                    onMouseDown={this.obterCoordenada}
                 >
                 </canvas>
             </div>
